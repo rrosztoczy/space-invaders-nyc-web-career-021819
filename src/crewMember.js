100fe58aba19757e271d9f439527b91029345f5a
rrosztoczy <rrosztoczy@gmail.com>
2019-07-18 10:26:36 -0600

    initial classes created, paramater tests passed

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,22 @@
-class CrewMember {}
+class CrewMember {
+
+constructor(position, currentShip='Looking for a Rig') {
+    this.position = position
+    this.currentShip = currentShip
+}
+
+engageWarpDrive() {
+    return this.currentShip === 'Looking for a Rig' ? 'had no effect' : 'used it!'
+}
+
+setsInvisibility() {
+    return this.currentShip === 'Looking for a Rig' ? 'had no effect' : 'used it!'
+}
+
+chargePhasers() {
+    return this.currentShip === 'Looking for a Rig' ? 'had no effect' : 'used it!'
+}
+
+
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,15 @@
-class Spaceship {}
+class Spaceship {
+
+    constructor(name, crew, phasers, shields, cloaked=false, warpDrive='disengaged', phasersCharge="uncharged") {
+        this.name = name
+        this.crew = crew
+        this.phasers = phasers
+        this. shields = shields
+        this.cloaked = cloaked
+        this.warpDrive = warpDrive
+        this.docked = this.crew.length > 0 ? false : true
+        this.phasersCharge = phasersCharge
+    }
+
+
+}
